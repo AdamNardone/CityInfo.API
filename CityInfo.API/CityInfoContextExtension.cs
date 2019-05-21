2fea8c46c868875a8a9e3dd29f2a47105629aef1
AdamNardone <48363859+AdamNardone@users.noreply.github.com>
2019-05-21 10:00:03 -0400

    Add files via upload

---
+++ CityInfo.API/CityInfoContextExtension.cs
@@ -0,0 +1,78 @@
+﻿using CityInfo.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public static class CityInfoContextExtension
+    {
+        public static void EnsureSeedDataForContext(this CityInfoContext context)
+        {
+            if (context.Cities.Any())
+                return;
+
+            var cities = new List<City>()
+            {
+                new City()
+                {
+                    Name = "New York City",
+                    Description = "The one with the big park.",
+                    PointsOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Central Park",
+                            Description = "Most visited park in the USA."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "Empire State Building",
+                            Description = "A big skyscraper in Manhattan."
+                        }
+                    }
+                },
+                new City()
+                {
+                    Name = "Antwerp",
+                    Description = "The one with the unfinished cathedra.l",
+                    PointsOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Cathedral of Our Lady",
+                            Description = "A gothic style cathedral."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "Antwerp Central Station",
+                            Description = "Finest example of railway architecture in Belgium."
+                        }
+                    }
+                },
+                new City()
+                {
+                    Name = "Paris",
+                    Description = "The one with the big tower.",
+                    PointsOfInterest = new List<PointOfInterest>()
+                    {
+                        new PointOfInterest()
+                        {
+                            Name = "Eiffel Tower",
+                            Description = "A wrought iron lattice tower on the Champ de Mars."
+                        },
+                        new PointOfInterest()
+                        {
+                            Name = "The Louvre",
+                            Description = "The world's largest museum."
+                        }
+                    }
+                }
+            };
+
+            context.Cities.AddRange(cities);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ CityInfo.API/Entities/CityInfoContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace CityInfo.API.Entities
+{
+    public class CityInfoContext : DbContext
+    {
+        public CityInfoContext(DbContextOptions<CityInfoContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public Microsoft.EntityFrameworkCore.DbSet<City> Cities { get; set; }
+        public Microsoft.EntityFrameworkCore.DbSet<PointOfInterest> PointsOfInterest { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("connectionString");
+
+
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
